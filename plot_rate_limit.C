0a44575d018696ee5d882ab04362ef3c06aa4392
Mark Wong <mark@squarepotato.com>
2023-05-05 08:53:17 +0200

    display measured/real

--- plot_rate_limit.C
+++ plot_rate_limit.C
@@ -7,21 +7,28 @@
  *
  */
 void plot_rate_limit() {
-    std::vector<float> reference = {1, 10, 20, 30, 60, 100, 200, 500};
-    std::map<std::string, std::vector<float> > _data = {
-        {"klaus6b10bitLG", {1, 13, 20, 35, 1040, 1040, 1040, 1040} },
-        {"twinpeaks", {0, 0, 0, 0, 0, 0, 0, 0} }, //missing one data point
-        {"citirocLG", {1, 1.7, 1.7, 1.7, 1.7, 1.7, 1.7, 1.7} },
-        {"tofpet2b", {0, 0, 0, 0, 0, 0, 0, 0} },
-        {"drs4", {0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8} }
+    std::vector<float> reference = {0.1, 0.2, 0.4, 0.75, 1, 10, 20, 30, 60, 100, 200, 500};
+    std::map<std::string, std::vector<float> > _measured = {
+        {"klaus6b10bitLG", {0, 0, 0, 0, 1, 13, 20, 35, 1040, 1040, 1040, 1040} },
+        {"twinpeaks", {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} }, //missing one data point
+        {"citirocLG", {0, 0, 0, 0, 1, 1.7, 1.7, 1.7, 1.7, 1.7, 1.7, 1.7} },
+        {"tofpet2b", {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} },
+        {"drs4", {0.1, 0.2, 0.7, 0.75, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8} }
     };
     std::map<std::string, std::vector<float> > _err = {
-        {"klaus6b10bitLG", {1, 1, 1, 1, 1, 1, 1, 1} },
-        {"twinpeaks", {1, 1, 1, 1, 1, 1, 0, 1} },
-        {"citirocLG", {1, 1, 1, 1, 1, 1, 1, 1} },
-        {"tofpet2b", {0, 0, 0, 0, 0, 0, 0, 0} },
-        {"drs4", {1, 1, 1, 1, 1, 1, 1, 1} }
+        {"klaus6b10bitLG", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} },
+        {"twinpeaks", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1} },
+        {"citirocLG", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} },
+        {"tofpet2b", {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} },
+        {"drs4", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} }
     };
+
+    std::map<std::string, std::vector<float> > _data;
+    for(std::map<std::string, std::vector<float> >::iterator it = _measured.begin(); it != _measured.end(); ++it) {
+        for(Int_t i=0; i<it->second.size(); ++i) {
+            _data[it->first].push_back(_measured[it->first][i]/reference[i]);
+        }
+    }
 
     TGraphErrors *g1 = new TGraphErrors(reference.size(), &reference[0], &_data["klaus6b10bitLG"][0], 0, &_err["klaus6b10bitLG"][0]);
     g1->SetTitle("Klaus6b 10bit LG");
@@ -45,15 +52,17 @@ void plot_rate_limit() {
     g5->SetLineColor(kBlack);
 
     TMultiGraph *mg = new TMultiGraph();
-    mg->SetTitle(";Real rate[kHz];Measured rate[kHz]");
-    mg->Add(g1, "AP");
-    mg->Add(g2, "AP");
-    mg->Add(g3, "AP");
-    mg->Add(g4, "AP");
-    mg->Add(g5, "AP");
+    mg->SetTitle(";Real rate[kHz];#frac{Measured}{Real}");
+    mg->Add(g1, "ALP");
+    mg->Add(g2, "ALP");
+    mg->Add(g3, "ALP");
+    mg->Add(g4, "ALP");
+    mg->Add(g5, "ALP");
+    mg->GetXaxis()->SetLimits(0.09, 600);
     TCanvas *c = new TCanvas("c", "c");
     mg->Draw("A");
     c->Draw();
+    c->SetLogx();
     TLegend *legend = c->BuildLegend(0.13, 0.66, 0.43, 0.87);
     legend->SetLineWidth(0);
     c->SaveAs("plot_rate_limit.pdf");
